c50bcd95b2f2637f5d17afe03a6cc57b3e3b5135
hilmikt <muhammedhilmi46@gmail.com>
2025-05-16 17:30:19 +0530

    added the function to store the generated addresses in an array

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -6,9 +6,10 @@ import {dogNumberCheck} from "./example_mapping.sol";
 // if it has to import more than one, but not all
 
 contract StorageFactory{
-    dogNumberCheck public DogNumberCheck;
+    dogNumberCheck[] public listOfDogNumberCheck;
 
     function createSimpleStorageContract() public {
-        DogNumberCheck = new dogNumberCheck();
+        dogNumberCheck newDogNumberCheck = new dogNumberCheck();
+        listOfDogNumberCheck.push(newDogNumberCheck);
     }
 }
