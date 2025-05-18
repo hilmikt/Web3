28e95c955dfd2eb8f0bb110582b09fa85e03ae3c
hilmikt <muhammedhilmi46@gmail.com>
2025-05-18 16:52:02 +0530

    learning simple storage ABI

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -6,10 +6,17 @@ import {dogNumberCheck} from "./example_mapping.sol";
 // if it has to import more than one, but not all
 
 contract StorageFactory{
-    dogNumberCheck[] public listOfDogNumberCheck;
+    //dogNumberCheck[] public listOfDogNumberCheck;
+    address[] public listOfSimpleStorageAddresses;
 
     function createSimpleStorageContract() public {
         dogNumberCheck newDogNumberCheck = new dogNumberCheck();
         listOfDogNumberCheck.push(newDogNumberCheck);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        // Address
+        // ABI - Application Binary Interface
+        SimpleStorage mySimpleStorage = listOfDogNumberCheck[_simpleStorageIndex];
     }
 }
