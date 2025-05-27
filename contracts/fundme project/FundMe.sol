f5101e7dce56f111c1ed27de7269005454aa40bf
Hilmi <muhammedhilmi46@gmail.com>
2025-05-27 22:33:55 +0530

    Updated Fundme.sol by adding chainlink interface

--- contracts/fundme project/FundMe.sol
+++ contracts/fundme project/FundMe.sol
@@ -6,6 +6,25 @@
 
 pragma solidity ^0.8.18;
 
+// solhint-disable-next-line interface-starts-with-i
+
+interface AggregatorV3Interface {
+  function decimals() external view returns (uint8);
+
+  function description() external view returns (string memory);
+
+  function version() external view returns (uint256);
+
+  function getRoundData(
+    uint80 _roundId
+  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+
+  function latestRoundData()
+    external
+    view
+    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+}
+
 contract FundMe {
 
     //uint256 public myValue = 1;
@@ -20,4 +39,15 @@ contract FundMe {
     }
 
     //function withdraw() public {}
+
+    function getPrice() public {
+        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // ABI
+    }
+    function getConversionRate() public {}
+
+    function getVersion() public view returns (uint256){
+        uint256 version = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+        return version;
+    }
 }
